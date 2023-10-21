dd433290f896d4be91ed286804125ffdd133a045
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-10-21 11:35:04 +0200

    Move the whole project to gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { StrictMode } from "react";
+import "../styles.css";
+
+import App from "./App";
+
+export default function HomePage() {
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
