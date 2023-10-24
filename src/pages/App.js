607c998952034b673da14132e5cb6b42d679f64b
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-10-24 08:31:20 +0200

    Code optimalization.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 function Square({ value, onSquareClick }) {
   return (
@@ -15,9 +15,9 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
     }
     const nextSquares = squares.slice();
     if (xIsNext) {
-      nextSquares[i] = 'X';
+      nextSquares[i] = "X";
     } else {
-      nextSquares[i] = 'O';
+      nextSquares[i] = "O";
     }
     onPlay(nextSquares);
   }
@@ -25,33 +25,37 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = 'Winner: ' + winner;
+    status = "Winner: " + winner;
+  } else if (currentMove === 9) {
+    status = "Tie";
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
-  else if (currentMove === 9) {
-    status = 'Tie';
-  }
-  else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+
+  const board = [];
+  for (let row = 0; row < 3; row++) {
+    const rowSquares = [];
+    for (let col = 0; col < 3; col++) {
+      const squareIndex = row * 3 + col;
+      rowSquares.push(
+        <Square
+          key={squareIndex}
+          value={squares[squareIndex]}
+          onSquareClick={() => handleClick(squareIndex)}
+        />
+      );
+    }
+    board.push(
+      <div key={row} className="board-row">
+        {rowSquares}
+      </div>
+    );
   }
 
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {board}
     </>
   );
 }
@@ -61,13 +65,11 @@ export default function Game() {
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
-  const gameEnd = false;
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    if (currentMove === 9) { gameEnd = true; }
   }
 
   function jumpTo(nextMove) {
@@ -83,25 +85,28 @@ export default function Game() {
     }
 
     if (move !== currentMove) {
-      listElement = (<button onClick={() => jumpTo(move)}>{description}</button>);
-    }
-    else {
-      if (move > 0) { description = `You are at move #${move}.`; }
-      else { description = "You are on game start.";}
-      listElement = (<p>{description}</p>);
+      listElement = <button onClick={() => jumpTo(move)}>{description}</button>;
+    } else {
+      if (move > 0) {
+        description = `You are at move #${move}.`;
+      } else {
+        description = "You are on game start.";
+      }
+      listElement = <p>{description}</p>;
     }
 
-    return (
-      <li key={move}>
-        {listElement}
-      </li>
-    );
-  })
+    return <li key={move}>{listElement}</li>;
+  });
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} currentMove={currentMove} />
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+          currentMove={currentMove}
+        />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
